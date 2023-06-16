d7fe708c06688d61210021bbe2048d44be6b7196
TRX <trx@TRXs-MacBook-Air.local>
2023-06-16 23:22:48 +0330

    add some comments to F1

--- Hardware/F1.py
+++ Hardware/F1.py
@@ -2,17 +2,30 @@ from all_memory import AllMemory
 
 class F1:
     def __init__(self, memory: AllMemory) -> None:
+        """
+        F1 is the first functional unit of the CPU. It is responsible for the following instructions:
+        000: NOP
+        001: ADD (AC <- AC + DR)
+        010: CLRAC (AC <- 0)
+        011: INCAC (AC <- AC + 1)
+        100: DRTAC (AC <- DR) 
+        101: DRTAR (AR <- DR[10:0])
+        110: PCTAR (AR <- PC)
+        111: WRITE (M[AR] <- DR)
+        """
         self.memory = memory
         self.opt = ['000', '001', '010', '011', '100', '101', '110', '111']
         self.asm = ['NOP', 'ADD', 'CLRAC', 'INCAC', 'DRTAC', 'DRTAR','PCTAR', 'WRITE']
         self.func = [self.isz_000, self.isz_001, self.isz_010, self.isz_011, self.isz_100, self.isz_101, self.isz_110, self.isz_111]
     
-    def instruction(self, inst):
+    def instruction(self, inst: str):
         for i in range(len(self.opt)):
             if inst == self.opt[i]:
+                # If the instruction matches an op code, call the corresponding function
                 self.func[i]()
                 break
             elif inst == self.asm[i]:
+                # If the instruction matches an assembly instruction, call the corresponding function
                 self.func[i]()
                 break
     
