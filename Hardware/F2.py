58b432978af60c979a86e7c8ff65b146ff84ca0a
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-06-19 16:36:18 +0330

    Edit F2 and fixed some bugs

--- Hardware/F2.py
+++ Hardware/F2.py
@@ -22,34 +22,78 @@ class F2:
     def isz_001(self):
         #AC <- AC - DR
         self.memory.AC.write(self.memory.AC.read_dec() - self.memory.DR.read_dec(), "d")
-        self.memory.flag["AC_flag"] += 1
 
     def isz_010(self):
         #AC <- AC | DR
         self.memory.AC.write(self.memory.AC.read_dec() | self.memory.DR.read_dec(), "d")
-        self.memory.flag["AC_flag"] += 1
 
     def isz_011(self):
         #AC <- AC & DR
         self.memory.AC.write(self.memory.AC.read_dec() & self.memory.DR.read_dec(), "d")
-        self.memory.flag["AC_flag"] += 1
 
     def isz_100(self):
         #DR <- M[AR]
-        self.memory.DR.write(self.memory.M[self.memory.AR.read_dec()], "d")
-        self.memory.flag["DR_flag"] += 1
+        self.memory.DR.write(self.memory.main_memory.read(self.memory.AR.read_dec(), 'd'), "d")
 
     def isz_101(self):
         #DR <- AC
         self.memory.DR.write(self.memory.AC.read_dec(), "d")
-        self.memory.flag["DR_flag"] += 1
 
     def isz_110(self):
         #DR <- DR + 1
         self.memory.DR.write(self.memory.DR.read_dec() + 1, "d")
-        self.memory.flag["DR_flag"] += 1
 
     def isz_111(self):
         #DR[10:0] <- PC
-        self.memory.DR.assign_bits(self.memory.PC.read_binary()[-10:], -10, len(self.memory.DR.read_binary()))
-        self.memory.flag["DR_flag"] += 1
+        self.memory.DR.assign_bits(self.memory.PC.read_binary(), 0, 10)
+
+
+if __name__ == '__main__':
+    # Initialize a new AllMemory object and a new F2 object
+    mem = AllMemory()
+    f2 = F2(mem)
+
+    # Test the NOP instruction
+    f2.instruction('000')
+    assert mem.AC.read_dec() == 0
+
+    # Test the SUB instruction
+    mem.AC.write(10, "d")
+    mem.DR.write(3, "d")
+    f2.instruction('001')
+    assert mem.AC.read_dec() == 7
+
+    # Test the OR instruction
+    mem.AC.write("0b1010", "b")
+    mem.DR.write("0b1100", "b")
+    f2.instruction('010')
+    assert mem.AC.read_binary() == "0b0000000000001110"
+
+    # Test the AND instruction
+    mem.AC.write("0b1010", "b")
+    mem.DR.write("0b1100", "b")
+    f2.instruction('011')
+    assert mem.AC.read_dec() == 0b1000
+
+    # Test the READ instruction
+    mem.main_memory.write(5, 42 ,'d')
+    mem.AR.write(5, "d")
+    f2.instruction('100')
+    assert mem.DR.read_dec() == 42
+
+    # Test the ACTDR instruction
+    mem.AC.write(10, "d")
+    f2.instruction('101')
+    assert mem.DR.read_dec() == 10
+
+    # Test the INCDR instruction
+    mem.DR.write(5, "d")
+    f2.instruction('110')
+    assert mem.DR.read_dec() == 6
+
+    # Test the PCTDR instruction
+    mem.PC.write("0b01010101010", "b")
+    f2.instruction('111')
+    assert mem.DR.read_binary()[-11:] == "01010101010"
+
+    print("All tests passed!")
