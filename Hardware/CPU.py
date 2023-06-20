bbe071a2cdf085ae12513be2bf8f4de6ad01f9ed
TRX <trx@TRXs-MacBook-Air.local>
2023-06-20 15:40:58 +0330

    finish the cpu

--- Hardware/CPU.py
+++ Hardware/CPU.py
@@ -5,13 +5,21 @@ from F3 import F3
 from CD_BR import CD, BR
 
 class CPU:
-    def __init__(self) -> None:
-        self.memory = AllMemory()
+    def __init__(self, memory=AllMemory()) -> None:
         self.f1 = F1(self.memory)
         self.f2 = F2(self.memory)
         self.f3 = F3(self.memory)
         self.CD = CD(self.memory)
         self.BR = BR(self.memory)
+        self.memory = memory
 
     def clock(self):
-        pass
+        self.memory.reset_flag()
+        opt = self.memory.control_memory.read(self.memory.CAR.read_dec(), 'b')[2:]
+        self.f1.insruction(opt[0:3])
+        self.f2.insruction(opt[3:6])
+        self.f3.insruction(opt[6:9])
+        self.CD.insruction(opt[9:11])
+        self.BR.insruction(opt[11:13])
+        if self.memory.check_flags():
+            raise("Error in CPU, Write in the same space twice in the same clock cycle")
